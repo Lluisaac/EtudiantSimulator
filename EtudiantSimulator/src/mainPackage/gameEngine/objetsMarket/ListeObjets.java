d6ed45626f47f5d809f2f07f16d36a74c9604a10
Lluisaac <isaaclluis@gmail.com>
2017-10-13 20:41:19 +0200

    Petits details

--- EtudiantSimulator/src/mainPackage/gameEngine/objetsMarket/ListeObjets.java
+++ EtudiantSimulator/src/mainPackage/gameEngine/objetsMarket/ListeObjets.java
@@ -30,15 +30,10 @@ public class ListeObjets {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
-String[] contentTab = content.split("\n");
 
-<<<<<<< HEAD
 		String[] contentTab = content.split("\n");
 
-=======
->>>>>>> 3effa97493ff1e3e19374f0411556f7e2a6bf9f3
-		for (int i = 0; i < contentTab.length-1; i++) {
+		for (int i = 0; i < contentTab.length - 1; i++) {
 
 			String[] objetsInfo = contentTab[i].split(";");
 
@@ -48,22 +43,15 @@ String[] contentTab = content.split("\n");
 			for (int j = 0; j < listeAttributs.length; j++) {
 				attributs[j] = Float.parseFloat(listeAttributs[j]);
 			}
-			
+
 			Boolean debloque;
-			
-<<<<<<< HEAD
-=======
-			String test= objetsInfo[3];
-			System.out.print(test);
-			System.out.println(test.equals("true"));
-			
->>>>>>> 3effa97493ff1e3e19374f0411556f7e2a6bf9f3
-			debloque=objetsInfo[3].equals("true");
-			
+
+			debloque = objetsInfo[3].equals("true");
+
 			if (objetsInfo[0].equals("Upgrade")) {
-				ListeObjets.listeObjets.add(new ObjetUpgrade(objetsInfo[1], attributs,debloque));
+				ListeObjets.listeObjets.add(new ObjetUpgrade(objetsInfo[1], attributs, debloque));
 			} else {
-				ListeObjets.listeObjets.add(new ObjetBonus(objetsInfo[1], attributs,debloque ));
+				ListeObjets.listeObjets.add(new ObjetBonus(objetsInfo[1], attributs, debloque));
 			}
 		}
 	}
@@ -72,17 +60,17 @@ String[] contentTab = content.split("\n");
 
 		return listeObjets;
 	}
-	
+
 	public static ArrayList<ObjetGeneral> getlisteObjetsDebloques() {
-		
+
 		ArrayList<ObjetGeneral> temp = new ArrayList<ObjetGeneral>();
-		
+
 		for (int i = 0; i < ListeObjets.listeObjets.size(); i++) {
 			if (ListeObjets.listeObjets.get(i).isDebloque()) {
 				temp.add(ListeObjets.listeObjets.get(i));
 			}
 		}
-		
+
 		return temp;
 	}
 
@@ -94,14 +82,14 @@ String[] contentTab = content.split("\n");
 	public static ObjetGeneral getObjetGeneral(int i) {
 		return listeObjets.get(i);
 	}
-	
+
 	public String toString() {
 		String r = "";
-		
+
 		for (int i = 0; i < ListeObjets.listeObjets.size(); i++) {
 			r += ListeObjets.listeObjets.get(i) + "\n";
 		}
-		
+
 		return r;
 	}
 }
